1a4f14db85e2cd1c0d1c0a475441c7e643a5cb5c
Khair Muhammad <khairmuhammadmemon@healthhub.kr>
2024-08-09 19:18:50 +0500

    #Added the findByTitleContaining and findByCategory methods and configured the DataRest which only allows GET methods

--- src/main/java/com/khair/library_app_backend/config/MyDataRestConfig.java
+++ src/main/java/com/khair/library_app_backend/config/MyDataRestConfig.java
@@ -1,5 +1,6 @@
 package com.khair.library_app_backend.config;
 
+import com.khair.library_app_backend.entity.Book;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -13,6 +14,26 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
-        HttpMethod[] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE};
+        HttpMethod[] theUnsupportedActions = {
+                HttpMethod.POST,
+                HttpMethod.PATCH,
+                HttpMethod.PUT,
+                HttpMethod.DELETE
+        };
+
+        config.exposeIdsFor(Book.class);
+
+        disableHttpMethods(Book.class, config, theUnsupportedActions);
+
+        /*Configure CORS Mapping*/
+        cors.addMapping(config.getBasePath() + "/**")
+                .allowedOrigins(theAllowedOrigins);
+    }
+
+    private void disableHttpMethods(Class<?> theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
+        config.getExposureConfiguration()
+                .forDomainType(theClass)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
     }
 }
--- src/main/java/com/khair/library_app_backend/dao/BookRepository.java
+++ src/main/java/com/khair/library_app_backend/dao/BookRepository.java
@@ -1,9 +1,15 @@
 package com.khair.library_app_backend.dao;
 
 import com.khair.library_app_backend.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
+    Page<Book> findByCategory(@RequestParam("category") String category, Pageable pageable);
 }
