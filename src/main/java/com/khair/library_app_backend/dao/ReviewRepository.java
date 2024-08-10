35be3c63804643c0c9d9354044456cd96c0ec8b4
Khair Muhammad <khairmuhammadmemon@healthhub.kr>
2024-08-10 14:04:38 +0500

    #Created the Review entity and its repository, added the configuration to expose Ids and allowing only GET methods in MyDataRestConfig class

---
+++ src/main/java/com/khair/library_app_backend/dao/ReviewRepository.java
@@ -0,0 +1,14 @@
+package com.khair.library_app_backend.dao;
+
+import com.khair.library_app_backend.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findByBookId(@RequestParam("book_id") Long bookId, Pageable pageable);
+}
