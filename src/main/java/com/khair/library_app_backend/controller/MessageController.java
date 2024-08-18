90a6032f44616ad63c6a026e16bb6b5c3b0197ef
Khair Muhammad <khairmuhammadmemon@healthhub.kr>
2024-08-18 16:55:02 +0500

    #Added the functionality for admin to view the messages from users/students and response to messages

--- src/main/java/com/khair/library_app_backend/controller/MessageController.java
+++ src/main/java/com/khair/library_app_backend/controller/MessageController.java
@@ -1,6 +1,7 @@
 package com.khair.library_app_backend.controller;
 
 import com.khair.library_app_backend.entity.Message;
+import com.khair.library_app_backend.requestmodels.AdminQuestionRequest;
 import com.khair.library_app_backend.service.MessagesService;
 import com.khair.library_app_backend.utils.ExtractJwt;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +24,17 @@ public class MessageController {
                             @RequestBody Message messageRequest) {
         String userEmail = ExtractJwt.payloadJwtExtraction(token, "\"sub\"");
         messagesService.postMessage(messageRequest, userEmail);
+    }
+
+    @PutMapping("/secure/admin/message")
+    public void putMessage(@RequestHeader(value = "Authorization") String token,
+                           @RequestBody AdminQuestionRequest adminQuestionRequest) throws Exception {
+        String userEmail = ExtractJwt.payloadJwtExtraction(token, "\"sub\"");
+
+        String admin = ExtractJwt.payloadJwtExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only.");
+        }
+        messagesService.putMessage(adminQuestionRequest, userEmail);
     }
 }
---
+++ src/main/java/com/khair/library_app_backend/requestmodels/AdminQuestionRequest.java
@@ -0,0 +1,11 @@
+package com.khair.library_app_backend.requestmodels;
+
+import lombok.Data;
+
+@Data
+public class AdminQuestionRequest {
+
+    private Long id;
+
+    private String response;
+}
