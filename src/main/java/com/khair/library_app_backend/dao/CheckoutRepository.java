8ec9839f629747b2db1f2cfb136aea8e5896c9bb
Khair Muhammad <khairmuhammadmemon@healthhub.kr>
2024-08-19 19:54:47 +0500

    #Added the functionality for admin to change the quantity of book and delete the book

--- src/main/java/com/khair/library_app_backend/dao/CheckoutRepository.java
+++ src/main/java/com/khair/library_app_backend/dao/CheckoutRepository.java
@@ -2,6 +2,9 @@ package com.khair.library_app_backend.dao;
 
 import com.khair.library_app_backend.entity.Checkout;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +13,10 @@ import java.util.List;
 public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
 
     Checkout findByUserEmailAndBookId(String userEmail, Long bookId);
+
     List<Checkout> findBooksByUserEmail(String userEmail);
+
+    @Modifying
+    @Query("delete from Checkout c where c.bookId in :book_id")
+    void deleteAllByBookId(@Param("book_id") Long bookId);
 }
--- src/main/java/com/khair/library_app_backend/dao/ReviewRepository.java
+++ src/main/java/com/khair/library_app_backend/dao/ReviewRepository.java
@@ -4,6 +4,9 @@ import com.khair.library_app_backend.entity.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,5 +14,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     Page<Review> findByBookId(@RequestParam("book_id") Long bookId, Pageable pageable);
+
     Review findByUserEmailAndBookId(String userEmail, Long bookId);
+
+    @Modifying
+    @Query("delete from Review r where r.bookId in :book_id")
+    void deleteAllByBookId(@Param("book_id") Long bookId);
 }
